091bca06ff203a9e4bdfbd572d79433a7cd224f4
howon-ryu <75923078+howon-ryu@users.noreply.github.com>
2021-11-21 16:56:07 +0900

    Create transaction.java

---
+++ transaction.java
@@ -0,0 +1,38 @@
+package atmnew;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class transaction {
+	
+	static ArrayList <String>log = new ArrayList<>();
+	
+	private String id ="";
+	private String cash = "";
+	
+	
+	public transaction()
+	{
+		
+	}
+	
+
+
+	public void add(String string) {
+		// TODO Auto-generated method stub
+		log.add(string);
+	}
+
+	public String get(int idx) {
+		// TODO Auto-generated method stub
+		return log.get(idx);
+	}
+
+	public int size() {
+		// TODO Auto-generated method stub
+		return log.size();
+		
+	}
+
+
